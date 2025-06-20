25ae39a7f418eadaae18c900786ae81f2e286c33
Your Name <59105428+nosebs@users.noreply.github.com>
2025-06-20 19:01:47 +0600

    cleanup

--- index.ts
+++ index.ts
@@ -5,16 +5,11 @@ import { FixGoogleAuthPatch } from "./patches/fixGoogleAuth";
 import { NoWatermarkPatch } from "./patches/noWatermark";
 console.log("Hello via Bun!");
 
-try {
-    console.log(Java.available)
-    Java.perform(() => {
-        [
-            new SpoofSimPatch(),
-            new DisableForcedLoginPatch(),
-            new FixGoogleAuthPatch(),
-            new NoWatermarkPatch()
-        ].forEach((patch) => patch.patch())
-    })
-} catch (e) {
-    console.log(e.stack)
-}
+Java.perform(() => {
+    [
+        new SpoofSimPatch(),
+        new DisableForcedLoginPatch(),
+        new FixGoogleAuthPatch(),
+        new NoWatermarkPatch()
+    ].forEach((patch) => patch.patch())
+})
