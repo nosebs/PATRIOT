a05205c955e6dc5a09ac9eee8eae256b5dcd85bb
Your Name <59105428+nosebs@users.noreply.github.com>
2025-06-21 14:46:20 +0600

    fix

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { DisableForcedLoginPatch } from "./patches/disableForcedLogin";
 import { FixGoogleAuthPatch } from "./patches/fixGoogleAuth";
 import { NoWatermarkPatch } from "./patches/noWatermark";
 import { RemoveDownloadRestrictionsPatch } from "./patches/removeDownloadRestrictions";
+import { FeedFilterPatch } from "./patches/feedFilter";
 console.log("Hello via Bun!");
 
 Java.perform(() => [
@@ -12,5 +13,6 @@ Java.perform(() => [
     new DisableForcedLoginPatch(),
     new FixGoogleAuthPatch(),
     new NoWatermarkPatch(),
-    new RemoveDownloadRestrictionsPatch()
+    new RemoveDownloadRestrictionsPatch(),
+    //new FeedFilterPatch()
 ].forEach((patch) => patch.patch()))
